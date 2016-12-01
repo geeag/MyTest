808bd935289a3c371ccbed50e501c7251c790ff0
geeag <geeag@qq.com>
2016-12-01 13:34:39 +0800

    my first commit

---
+++ MyFirstMavenProject/src/main/java/com/ideal/test/Main.java
@@ -0,0 +1,10 @@
+package com.ideal.test;
+
+/**
+ * Created by geeag on 16/12/1.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
